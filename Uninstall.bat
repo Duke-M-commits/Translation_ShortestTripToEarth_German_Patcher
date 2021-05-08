b69e51357c7192bbc20cde9947a618688e01dfe2
DukeDukeEm <duke_duke_em@pm.me>
2021-05-08 02:08:40 +0200

    formatting

--- Uninstall.bat
+++ Uninstall.bat
@@ -9,9 +9,9 @@ IF EXIST "data.unity3d.backup" (
   RENAME data.unity3d.backup data.unity3d
 )
 ECHO Deleting patchfiles...
-FOR %%a in (*.assets) DO DEL %%a
+FOR %%a IN (*.assets) DO DEL %%a
 ECHO Deleting more patchfiles...
-FOR /l %%i in (0,1,1) DO IF EXIST level%%i (DEL level%%i)
+FOR /l %%i IN (0,1,9) DO IF EXIST level%%i (DEL level%%i)
 ECHO Deleting even more patchfiles...
 IF EXIST "globalgamemanagers" (
   DEL "globalgamemanagers"
@@ -25,17 +25,17 @@ CD ..\..\AssetBundles
 ECHO Reverting patched Base Game...
 IF EXIST "base.ab.backup" (
   DEL "base.ab"
-  RENAME base.ab.backup base.ab
+  RENAME "base.ab.backup" "base.ab"
 )
 ECHO Reverting patched DLC - The Old Enemies...
 IF EXIST "dlc2.ab.backup" (
   DEL "dlc2.ab"
-  RENAME dlc2.ab.backup dlc2.ab
+  RENAME "dlc2.ab.backup" "dlc2.ab"
 )
 ECHO Reverting patched DLC - Supporters Pack...
 IF EXIST "sdlc.ab.backup" (
   DEL "sdlc.ab"
-  RENAME sdlc.ab.backup sdlc.ab
+  RENAME "sdlc.ab.backup" "sdlc.ab"
 )
 CD %mod-dir%
 ECHO Done!
