4bc7d2fb915ae45200b56015bd0825448d96b633
DukeDukeEm <duke_duke_em@pm.me>
2021-05-14 16:01:32 +0200

    fixed gog install detection

--- Install.bat
+++ Install.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 SET mod-dir=%cd%
-SET patch-dir=%mod-dir%\Patch
+SET patch-dir="%mod-dir%\Patch\"
 CD %patch-dir%
 IF EXIST "UnityEX.exe" (
-  SET patch-app="%patch-dir%\UnityEX.exe"
+  SET patch-app=%patch-dir%\UnityEX.exe
 ) ELSE (
   ECHO Anscheinend hat dein Antivirus den Patcher gegessen...
   ECHO Ausstieg...
@@ -11,7 +11,7 @@ IF EXIST "UnityEX.exe" (
   EXIT
 )
 ECHO Version erkennen...
-CD %mod-dir%\..\..\..\..
+CD "%mod-dir%\..\..\..\.."
 IF EXIST "common\" (
   ECHO Steam Version erkannt...
   CD common
@@ -25,40 +25,40 @@ IF EXIST "common\" (
     EXIT
   )
 )
-CD Shortest Trip to Earth\Data
+CD "Shortest Trip to Earth\Data"
 ECHO Spieldateien exportieren...
 IF EXIST "data.unity3d" (
   IF NOT EXIST "data.unity3d.backup" (
     RENAME "data.unity3d" "data.unity3d.backup"
   )
 )
-"%patch-app%" exportbundle "data.unity3d.backup" -p "%cd%" >nul
+%patch-app% exportbundle "data.unity3d.backup" -p "%cd%" >nul
 ECHO Spieldateien modifizieren...
-FOR %%a IN (*.assets) DO "%patch-app%" import "%%a" -p "%patch-dir%" >nul
+FOR %%a IN (*.assets) DO %patch-app% import "%%a" -p %patch-dir% >nul
 ECHO Mehr Modifikationen...
-FOR /l %%i IN (0,1,9) DO IF EXIST "level%%i" "%patch-app%" import "level%%i" -p "%patch-dir%" >nul
-CD ..\AssetBundles
-ECHO Importiere modifizierte Haupt-Spieldateien
+FOR /l %%i IN (0,1,9) DO IF EXIST "level%%i" %patch-app% import "level%%i" -p %patch-dir% >nul
+CD "..\AssetBundles"
+ECHO Importiere modifizierte Haupt-Spieldateien...
 IF EXIST "base.ab" (
   IF NOT EXIST "base.ab.backup" (
     COPY "base.ab" "base.ab.backup" >nul
   )
 )
-"%patch-app%" import "base.ab" -mb_new -ncomp -f "%patch-dir%" >nul
+%patch-app% import "base.ab" -mb_new -ncomp -f %patch-dir% >nul
 ECHO Importiere modifizierte DLC-Spieldateien - The Old Enemies...
 IF EXIST "dlc2.ab" (
   IF NOT EXIST "dlc2.ab.backup" (
     COPY "dlc2.ab" "dlc2.ab.backup" >nul
   )
 )
-"%patch-app%" import "dlc2.ab" -mb_new -f "%patch-dir%" >nul
+%patch-app% import "dlc2.ab" -mb_new -f %patch-dir% >nul
 ECHO Importiere modifizierte DLC-Spieldateien - Supporters Pack...
 IF EXIST "sdlc.ab" (
   IF NOT EXIST "sdlc.ab.backup" (
     COPY "sdlc.ab" "sdlc.ab.backup" >nul
   )
 )
-"%patch-app%" import "sdlc.ab" -mb_new -f "%patch-dir%" >nul
+%patch-app% import "sdlc.ab" -mb_new -f %patch-dir% >nul
 CD %mod-dir%
 ECHO Done!
 TIMEOUT /t 8
